c18acc1d872cd52f49edb6d69edada580e1e343f
jaclynflomen <j_floman@fanshaweonline.ca>
2018-10-19 11:56:31 -0400

    files from class

---
+++ includes/connect.php
@@ -0,0 +1,47 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "root";
+$db = "db_cooperinfo";
+
+$conn = mysqli_connect($host, $user, $password, $db);
+
+if (!$conn) {
+    echo "something broke... connection isn't working";
+    exit;
+}
+
+//echo "connected!";
+//comment ^ out once seeing that it works
+
+
+//go and get all data from the database
+//$myQuery = "SELECT * FROM mainmodel";     commenting out to isolate query for one item below
+//$result = mysqli_query($conn, $myQuery);
+//$rows = array ();
+
+//fill the array with the result set and send it to the browser
+//while($row = mysqli_fetch_assoc($result)) {
+//    $rows[] = $row;
+//}
+
+//get one item from database
+if (isset($_GET["modelNo"])) {
+    $car = $_GET["modelNo"];
+
+    $myQuery = "SELECT * FROM mainmodel WHERE model='$car'";
+
+    $result = mysqli_query($conn, $myQuery);
+    $rows = array ();
+
+    //fill the array with the result set and send it to the browser
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+}
+}
+
+//encode the result and send it back
+echo json_encode($rows);
+
+
+?>
---
+++ js/master.js
@@ -0,0 +1,30 @@
+(() => {
+
+    function getData(){
+        let targetURL = "includes/connect.php?modelNo=R58";
+
+        fetch (targetURL) // go get the data and bring it back! good doggy
+        .then(res => res.json()) //turn the result into a plain JS object
+        .then(data => {
+            console.log(data);
+            //run a function to parse our data
+            showCarData(data[0]);
+        }) //lets see what we got
+        .catch(function(error) {
+            console.log(error); //if anything broke, log it to the console
+        });
+    }
+
+    function showCarData(data) {
+        //debugger;
+        //parse the DB info and put it where it needs to go
+        const{ modelName, pricing, modelDetails } = data; //destructuring assignment => MDN JS destructuring 
+
+        //grab the elements we need, and populate them with data
+        document.querySelector('.modelName').textContent = modelName
+        document.querySelector('.priceInfo').textContent = `$ ${pricing}.00`;
+        document.querySelector('.modelDetails').textContent = modelDetails
+    }
+
+    getData(); //trigger the getData function
+})();
