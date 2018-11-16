c46f2be047414137ab1e23f81072906068ba18f7
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-16 11:39:10 -0500

    preloader on click while fetching

--- js/master.js
+++ js/master.js
@@ -14,6 +14,8 @@ const vm = new Vue({
     mounted : function(){
         console.log('vue is ready to go on the page');
 
+        this.addPreloader(document.querySelector('.modelInfo'));
+
         //trigger an ajax call with a mocked click event
         document.querySelector('#F55').click();
     },
@@ -24,10 +26,38 @@ const vm = new Vue({
 
     updated : function() {
         console.log('things are different now');
+        //move the preloader out of the element and hide it
+        let preloader = document.querySelector('.preloader-wrapper');
+
+        //hide the preloader with css
+        //mpve it to the bottom of the page - ready for the next AJAX call
+        setTimeout(function(){
+            preloader.classList.add('hidden');
+            document.body.appendChild(preloader);
+        }, 1000); //1000m = 1second
+
     },
 
     methods : {
+
+        addPreloader(parentEl) {
+            //load the preloader into the parent element and make it draw
+            let preloader = document.querySelector('.preloader-wrapper');
+
+            parentEl.appendChild(preloader);
+
+            let animItem = bodymovin.loadAnimation ({
+                wrapper : document.querySelector('.preloader'),
+                animType : 'svg',
+                loop : true,
+                path : 'data/search.json'
+            })
+        },
+
         getData(e) {
+            //trigger the preloader
+            this.addPreloader(document.querySelector('.modelInfo'));
+            let preloader = document.querySelector('.preloader-wrapper').classList.remove('hidden');
             //debugger;
             let targetURL = `./includes/connect.php?modelNo=${e.currentTarget.id}`;
              //gets the id of the element via the event object
